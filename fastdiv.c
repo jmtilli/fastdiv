f654496520ca94c9a321d95fb4f094ea4237f1c8
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2018-04-03 00:05:34 +0300

    Division and more testing

--- fastdiv.c
+++ fastdiv.c
@@ -24,6 +24,10 @@ void init_fastdivctx(struct fastdivctx *ctx, uint32_t divisor)
 {
   uint8_t ilogd = ilog(divisor);
   int power_of_2 = (divisor & (divisor - 1)) == 0;
+  if (divisor == 0 || divisor >= (1U<<31))
+  {
+    abort(); // Not supported
+  }
   if (power_of_2)
   {
     ctx->shift1 = 0;
@@ -50,12 +54,61 @@ uint32_t fastmod(struct fastdivctx *ctx, uint32_t eax)
   return eaxorig - edx;
 }
 
+uint32_t fastdiv(struct fastdivctx *ctx, uint32_t eax)
+{
+  uint64_t edxeax = ((uint64_t)eax) * ctx->mult;
+  uint32_t edx = edxeax>>32;
+  uint32_t eaxorig = eax;
+  eax -= edx;
+  eax >>= (ctx->shift1);
+  eax += edx;
+  eax >>= (ctx->shift2);
+  return eax;
+}
+
 int main(int argc, char **argv)
 {
   struct fastdivctx ctx;
   uint32_t divisor;
   struct timeval tv1, tv2;
   int i, j;
+  uint32_t special_divisors[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,  (1U<<31) - 1};
+  uint32_t special_numbers[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                                (1U<<31) - 3, (1U<<31) - 2, (1U<<31) - 1,
+                                (1U<<31), (1U<<31) + 1, (1U<<31) + 2,
+                                (1ULL<<32) - 3, (1ULL<<32) - 2, (1ULL<<32) - 1};
+
+
+  for (i = 0; i < sizeof(special_divisors)/sizeof(*special_divisors); i++)
+  {
+    divisor = special_divisors[i];
+    init_fastdivctx(&ctx, divisor);
+    for (j = 0; j < 10000; j++)
+    {
+      uint32_t x;
+      x = ((rand()&1)<<31) | rand();
+      if (fastmod(&ctx, x) != (x % divisor))
+      {
+        printf("failure: %u %% %u\n", x, divisor);
+      }
+      if (fastdiv(&ctx, x) != (x / divisor))
+      {
+        printf("failure: %u %% %u\n", x, divisor);
+      }
+    }
+    for (j = 0; j < sizeof(special_numbers)/sizeof(*special_numbers); j++)
+    {
+      uint32_t x = special_numbers[j];
+      if (fastmod(&ctx, x) != (x % divisor))
+      {
+        printf("failure: %u %% %u\n", x, divisor);
+      }
+      if (fastdiv(&ctx, x) != (x / divisor))
+      {
+        printf("failure: %u %% %u\n", x, divisor);
+      }
+    }
+  }
   for (i = 0; i < 10000; i++)
   {
     divisor = rand();
@@ -68,9 +121,27 @@ int main(int argc, char **argv)
       {
         printf("failure: %u %% %u\n", x, divisor);
       }
+      if (fastdiv(&ctx, x) != (x / divisor))
+      {
+        printf("failure: %u %% %u\n", x, divisor);
+      }
+    }
+    for (j = 0; j < sizeof(special_numbers)/sizeof(*special_numbers); j++)
+    {
+      uint32_t x = special_numbers[j];
+      if (fastmod(&ctx, x) != (x % divisor))
+      {
+        printf("failure: %u %% %u\n", x, divisor);
+      }
+      if (fastdiv(&ctx, x) != (x / divisor))
+      {
+        printf("failure: %u %% %u\n", x, divisor);
+      }
     }
   }
+
   printf("Verified successfully\n");
+
   init_fastdivctx(&ctx, 37);
   gettimeofday(&tv1, NULL);
   for (i = 0; i < 1000*1000*1000/4; i++)
@@ -94,7 +165,7 @@ int main(int argc, char **argv)
     }
   }
   gettimeofday(&tv2, NULL);
-  printf("fast: %g nsec\n", tv2.tv_sec - tv1.tv_sec + (tv2.tv_usec-tv1.tv_usec)/1e6);
+  printf("fast mod: %g nsec\n", tv2.tv_sec - tv1.tv_sec + (tv2.tv_usec-tv1.tv_usec)/1e6);
 
   gettimeofday(&tv1, NULL);
   volatile uint32_t d = 37;
@@ -119,7 +190,57 @@ int main(int argc, char **argv)
     }
   }
   gettimeofday(&tv2, NULL);
-  printf("direct: %g nsec\n", tv2.tv_sec - tv1.tv_sec + (tv2.tv_usec-tv1.tv_usec)/1e6);
+  printf("direct mod: %g nsec\n", tv2.tv_sec - tv1.tv_sec + (tv2.tv_usec-tv1.tv_usec)/1e6);
+  gettimeofday(&tv1, NULL);
+  for (i = 0; i < 1000*1000*1000/4; i++)
+  {
+    volatile uint32_t x = i + 1;
+    if (fastdiv(&ctx, x) == x)
+    {
+      abort();
+    }
+    if (fastdiv(&ctx, x) == x)
+    {
+      printf("2\n");
+      abort();
+    }
+    if (fastdiv(&ctx, x) == x)
+    {
+      printf("3\n");
+      abort();
+    }
+    if (fastdiv(&ctx, x) == x)
+    {
+      printf("4\n");
+      abort();
+    }
+  }
+  gettimeofday(&tv2, NULL);
+  printf("fast div: %g nsec\n", tv2.tv_sec - tv1.tv_sec + (tv2.tv_usec-tv1.tv_usec)/1e6);
+
+  gettimeofday(&tv1, NULL);
+  for (i = 0; i < 1000*1000*1000/4; i++)
+  {
+    volatile uint32_t x = i + 1;
+    if (x / d == x)
+    {
+      abort();
+    }
+    if (x / d == x)
+    {
+      abort();
+    }
+    if (x / d == x)
+    {
+      abort();
+    }
+    if (x / d == x)
+    {
+      abort();
+    }
+  }
+  gettimeofday(&tv2, NULL);
+  printf("direct div: %g nsec\n", tv2.tv_sec - tv1.tv_sec + (tv2.tv_usec-tv1.tv_usec)/1e6);
   gettimeofday(&tv1, NULL);
   for (i = 0; i < 1000*1000*1000/4; i++)
   {
